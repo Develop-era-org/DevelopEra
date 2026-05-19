ff6a92dfbc22cab8a8013099c4094b16aa7a9a1f
chandu14321 <63911604+chandu14321@users.noreply.github.com>
2026-05-19 19:48:26 +0530

    changed the db and added the logs for db

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -5,6 +5,7 @@ console.log("Connecting to MongoDB...");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
