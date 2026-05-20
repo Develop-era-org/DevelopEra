bf0e229cb3d58d3ab25854b0d264292b213674dd
chandu14321 <63911604+chandu14321@users.noreply.github.com>
2026-05-20 17:03:37 +0530

    contact form fix input-fields

--- Backend/index.js
+++ Backend/index.js
@@ -6,45 +6,46 @@ dotenv.config();
 
 import connectDB from "./db/db.js";
 import FormRoute from "./routes/FormRoute.js";
+
 const port = process.env.PORT || 3000;
 
 // connect DB
 connectDB();
 
-//environment config
-dotenv.config();
-
-//express app
+// express app
 const app = express();
 
-//rate limiter
+// trust proxy
 app.set("trust proxy", 1);
 
-//json parser
+// json parser
 app.use(express.json());
 
-//cors
+// cors
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
   }),
 );
-console.log("CORS configured f or: ", process.env.FRONTEND_URL);
-// Global error handling middleware
-app.use((err, req, res, next) => {
-  console.error("GLOBAL ERROR:", err);
 
-  res.status(500).json({
-    success: false,
-    message: err.message,
-  });
-});
-//Main router
+console.log("CORS configured for:", process.env.FRONTEND_URL);
+
+// routes
 app.use("/", FormRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// global error middleware MUST be last
+// app.use((err, req, res, next) => {
+//   console.error("GLOBAL ERROR:", err);
+
+//   res.status(500).json({
+//     success: false,
+//     message: err.message || "Server Error",
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- Backend/middlewares/ratelimiter/formLimiter.js
+++ Backend/middlewares/ratelimiter/formLimiter.js
@@ -1,16 +1,22 @@
 import rateLimit from "express-rate-limit";
 
 const formLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 3, // max 3 requests
+  windowMs: 5 * 60 * 1000,
 
-  message: {
-    success: false,
-    error: "Too many form submissions. Please wait 5 minutes.",
-  },
+  max: 3,
 
   standardHeaders: true,
+
   legacyHeaders: false,
+
+  handler: (req, res) => {
+    console.log("BLOCKED:", req.ip);
+
+    res.status(429).json({
+      success: false,
+      error: "Too many form submissions. Please wait 5 minutes.",
+    });
+  },
 });
 
 export default formLimiter;
